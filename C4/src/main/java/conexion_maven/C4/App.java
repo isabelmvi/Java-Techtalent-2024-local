3fe5487ec39b656a5319cefc178784e020aa0c56
isabelmvi <isabel.murariu@gmail.com>
2024-05-20 11:11:03 +0200

    resto_T18

---
+++ C4/src/main/java/conexion_maven/C4/App.java
@@ -0,0 +1,13 @@
+package conexion_maven.C4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- Techtalent2024/src/module-info.java
+++ Techtalent2024/src/module-info.java
@@ -6,4 +6,5 @@
  */
 module Techtalent2024 {
 	requires java.desktop;
+	requires java.sql;
 }
