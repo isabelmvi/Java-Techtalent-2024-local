479c3bec3e1e3d5cf95b5ed9c8f43c13492d2bce
isabelmvi <isabel.murariu@gmail.com>
2024-06-10 13:38:00 +0200

    resto

---
+++ ud21/src/main/java/calculadora/prueba_calculadora.java
@@ -0,0 +1,14 @@
+package calculadora;
+
+public class prueba_calculadora {
+
+	public class Calc {
+	    public int add(int a, int b) {
+	        return a + b;
+	    }
+
+	    public int subtract(int a, int b) {
+	        return a - b;
+	    }
+	}
+}
