abc2be8e8ce7656965d09175042a0b637c39336d
isabelmvi <isabel.murariu@gmail.com>
2024-07-01 10:40:30 +0200

    resto

---
+++ C5_Frontend/proyecto-final-techtalent/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,HomeComponent,HeaderComponent,ServicesComponent,FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'proyecto-final-techtalent';
+}
