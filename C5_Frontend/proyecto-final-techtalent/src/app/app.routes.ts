283162e4f96eb4f407e245a4c92d302fbd258b31
isabelmvi <isabel.murariu@gmail.com>
2024-07-10 14:01:29 +0200

    ultima-modificacion-angular

--- C5_Frontend/proyecto-final-techtalent/src/app/app.routes.ts
+++ C5_Frontend/proyecto-final-techtalent/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ServicesComponent } from './services/services.component';
+import { HomeComponent } from './home/home.component';
+import { SobreNosotrosComponent } from './pages/sobre-nosotros/sobre-nosotros.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'servicios-digitales', component: ServicesComponent },
+    {path: 'home', component: HomeComponent },
+    {path: 'sobre-nosotros', component: SobreNosotrosComponent }
+
+];
--- C5_Frontend/proyecto-final-techtalent/src/app/home/home.component.ts
+++ C5_Frontend/proyecto-final-techtalent/src/app/home/home.component.ts
@@ -8,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  // currentSlide:number = 0;
+  // const slides: any = document.querySelectorAll('.slide');
+  // const totalSlides = this.slides.length;
 
+  // showSlide(index: number) {
+  //     const slider: any = document.querySelector('.slider');
+  //     slider.style.transform = `translateX(-${index * 100}%)`;
+  // }
+
+  // nextSlide() {
+  //     this.currentSlide = (this.currentSlide + 1) % this.totalSlides;
+  //     this.showSlide(this.currentSlide);
+  // }
+
+  // document.addEventListener('DOMContentLoaded', () => {
+  //     showSlide(currentSlide);
+  //     setInterval(nextSlide, 3000); // Moves to the next slide every 3 seconds
+  // });
+
+  // // Add this script to handle the navbar contraction
+  // const navbar = document.querySelector('.navbar');
+  // const navLinks = document.querySelector('.nav-links');
+  // const authButtons = document.querySelector('.auth-buttons');
+
+  // window.addEventListener('scroll', () => {
+  //     if (window.scrollY > 50) {
+  //         navLinks.classList.add('hidden');
+  //         authButtons.classList.add('hidden');
+  //         navbar.classList.add('shrink');
+  //     } else {
+  //         navLinks.classList.remove('hidden');
+  //         authButtons.classList.remove('hidden');
+  //         navbar.classList.remove('shrink');
+  //     }
+  // });
 }
