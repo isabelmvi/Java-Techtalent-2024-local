8f56b09f6702bd41932b0dea1ee0598bfb87050a
isabelmvi <isabel.murariu@gmail.com>
2024-07-11 12:56:19 +0200

    new_changes

--- C5_Frontend/proyecto-final-techtalent/src/app/app.routes.ts
+++ C5_Frontend/proyecto-final-techtalent/src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { ServicesComponent } from './services/services.component';
 import { HomeComponent } from './home/home.component';
 import { SobreNosotrosComponent } from './pages/sobre-nosotros/sobre-nosotros.component';
+import { ColaboradoresComponent } from './pages/colaboradores/colaboradores.component';
+import { ProductosComponent } from './pages/productos/productos.component';
 
 export const routes: Routes = [
     {path: 'servicios-digitales', component: ServicesComponent },
     {path: 'home', component: HomeComponent },
-    {path: 'sobre-nosotros', component: SobreNosotrosComponent }
+    {path: 'sobre-nosotros', component: SobreNosotrosComponent },
+    {path: 'productos', component: ProductosComponent},
+    {path: 'colaboradores', component: ColaboradoresComponent}
 
 ];
